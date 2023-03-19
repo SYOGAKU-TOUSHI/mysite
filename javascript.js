8452b3baa9a4bb491998ecd1c23ed228582f9c9f
shogaku <fw_yasu11@hotmail.com>
2023-03-20 00:06:45 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,38 +1,67 @@
-var player = {
-	hp: 100,
-	attack: 10,
-	defense: 5
-};
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-var enemy = {
-	hp: 50,
-	attack: 8,
-	defense: 3
-};
+const playerWidth = 20;
+const playerHeight = 20;
+const playerSpeed = 5;
+let playerX = (canvas.width - playerWidth) / 2;
+let playerY = canvas.height - playerHeight - 10;
 
-function attack() {
-	var damageToEnemy = player.attack - enemy.defense;
-	var damageToPlayer = enemy.attack - player.defense;
-	enemy.hp -= damageToEnemy;
-	player.hp -= damageToPlayer;
-	updateStats();
-	checkGameOver();
+const bulletWidth = 5;
+const bulletHeight = 10;
+const bullets = [];
+
+function drawPlayer() {
+  ctx.beginPath();
+  ctx.rect(playerX, playerY, playerWidth, playerHeight);
+  ctx.fillStyle = "#00FF00";
+  ctx.fill();
+  ctx.closePath();
 }
 
-function updateStats() {
-	document.getElementById("player-hp").innerHTML = player.hp;
-	document.getElementById("player-attack").innerHTML = player.attack;
-	document.getElementById("player-defense").innerHTML = player.defense;
-	document.getElementById("enemy-hp").innerHTML = enemy.hp;
-	document.getElementById("enemy-attack").innerHTML = enemy.attack;
-	document.getElementById("enemy-defense").innerHTML = enemy.defense;
+function drawBullet(x, y) {
+  ctx.beginPath();
+  ctx.rect(x, y, bulletWidth, bulletHeight);
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fill();
+  ctx.closePath();
 }
 
-function checkGameOver() {
-	if (player.hp <= 0) {
-		alert("Game over! You lost.");
-	}
-	else if (enemy.hp <= 0) {
-		alert("Congratulations! You won!");
-	}
+function updateBullets() {
+  for (let i = 0; i < bullets.length; i++) {
+    bullets[i].y -= 5;
+    if (bullets[i].y < 0) {
+      bullets.splice(i, 1);
+    }
+  }
 }
+
+function drawBullets() {
+  for (const bullet of bullets) {
+    drawBullet(bullet.x, bullet.y);
+  }
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPlayer();
+  updateBullets();
+  drawBullets();
+  requestAnimationFrame(gameLoop);
+}
+
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowLeft":
+      playerX = Math.max(playerX - playerSpeed, 0);
+      break;
+    case "ArrowRight":
+      playerX = Math.min(playerX + playerSpeed, canvas.width - playerWidth);
+      break;
+    case " ":
+      bullets.push({ x: playerX + playerWidth / 2 - bulletWidth / 2, y: playerY });
+      break;
+  }
+});
+
+gameLoop();
