388bd01d651367dd46b85b3a000ef66f84b82d52
shogaku <fw_yasu11@hotmail.com>
2023-03-19 20:57:18 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -21,4 +21,18 @@ function attack() {
 
 function updateStats() {
 	document.getElementById("player-hp").innerHTML = player.hp;
-	document.getElementById("
+	document.getElementById("player-attack").innerHTML = player.attack;
+	document.getElementById("player-defense").innerHTML = player.defense;
+	document.getElementById("enemy-hp").innerHTML = enemy.hp;
+	document.getElementById("enemy-attack").innerHTML = enemy.attack;
+	document.getElementById("enemy-defense").innerHTML = enemy.defense;
+}
+
+function checkGameOver() {
+	if (player.hp <= 0) {
+		alert("Game over! You lost.");
+	}
+	else if (enemy.hp <= 0) {
+		alert("Congratulations! You won!");
+	}
+}
